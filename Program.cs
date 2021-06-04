3aab1a45f9699eac3dfab1867d6044e9bb774cdb
Venkat <vramana98@gmail.com>
2021-06-04 07:27:50 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Google
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validator validator = new Validator();
+            Console.WriteLine(validator.DetectBadWord("vramanaasshole@gmail.com"));
+        }
+    }
+}
